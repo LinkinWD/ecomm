e0e087d13b1340eb33a7adc99011d7756626175b
yaram <yaramodesu@gmail.com>
2021-04-13 15:17:49 +0300

    paluu sekavuudesta, saakelin kirjoitusvirheet

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
-const express = require ('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
-const app = express()
+const app = express();
 
-const log = console.log
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['lkasld235j']
+  })
+);
+app.use(authRouter);
+app.use(productsRouter);
 
-app.use(express.urlencoded({
-    extended: true
-  }));
-app.get('/', (req, res) => {
-    res.send(`
-    <h1>hi there<h1>
-    <form method="POST">
-    <label for="email">email</label>
-    <input name="email"><br>
-    <label for="name">Salasana</label>
-    <input name="password"><br>
-    <label for="name">Confirm</label>
-    <input name="confirmPassword"><br>
-    <button>submit</button>
-    </form>`
-    
-    )
-})
-app.post('/', (req, res) => {
-    
-        log(req.body)
-    
-   
-})
-
-app.listen (3000, () => {
-    log('serveri rulaa')
-})
-
+app.listen(3000, () => {
+  console.log('Listening');
+});
