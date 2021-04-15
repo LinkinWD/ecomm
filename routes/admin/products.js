b405b9502fd4a2f5c9be84a500ef2b9d0cdd427e
yaram <yaramodesu@gmail.com>
2021-04-15 08:39:40 +0300

    vähän muhun liitymätön tehtävä ja vanhahko, skippaan. Palaan jos tarvii

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,23 +1,34 @@
 const express = require('express');
-const { validationResult } = require('express-validator')
+const multer = require('multer')
+
+const {handleErrors} = require('./middlewares') 
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const productsIndexTemplate = require('../../views/admin/products')
 const {requirePrice, requireTitle } = require('./validators')
 
 const router = express.Router();
+const upload = multer({storege: multer.memoryStorage()})
 
-router.get('/admin/products', (req, res) => {});
+router.get('/admin/products', async (req, res) => {
+  const products = await productsRepo.getAll();
+  res.send(productsIndexTemplate({products}))
+});
 
 router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [requirePrice, requireTitle], (req, res) => {
-  const errors = validationResult(req)
-  
-  
-
-  res.send('Submitted')
+router.post('/admin/products/new',
+upload.single('image'),
+[requirePrice, requireTitle],
+handleErrors(productsNewTemplate), 
+async (req, res) => {
+ 
+  const image = req.file.buffer.toString('base64')
+  const { title, price } = req.body
+  await productsRepo.create({title, price, image})
+  res.redirect('/admin/products')
 })
 
 module.exports = router;
